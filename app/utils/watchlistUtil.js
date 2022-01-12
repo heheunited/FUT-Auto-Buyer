fda0bf476699d32a937c54df3ef5b099be590fe7
heheunited <lycantrop11@gmail.com>
2022-01-13 01:10:45 +0200

    added waiting time

--- app/utils/watchlistUtil.js
+++ app/utils/watchlistUtil.js
@@ -65,9 +65,7 @@ export const watchListUtil = function (buyerSetting) {
 
                 for (var i = 0; i < outBidItems.length; i++) {
                   const currentItem = outBidItems[i];
-                  const waitingTime = i === 0
-                      ? i + 2
-                      : i;
+                  const waitingTime = i === 0 ? 1 : i;
                   let auction = currentItem._auction;
                   let currentBid = auction.currentBid || auction.startingBid;
 

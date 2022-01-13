df74466dbf747d70e991d0601e2deb785dd9e0c9
heheunited <lycantrop11@gmail.com>
2022-01-14 01:18:59 +0200

    edit delay

--- app/utils/watchlistUtil.js
+++ app/utils/watchlistUtil.js
@@ -65,7 +65,7 @@ export const watchListUtil = function (buyerSetting) {
 
                 for (var i = 0; i < outBidItems.length; i++) {
                   const currentItem = outBidItems[i];
-                  const waitingTime = i === 0 ? 1 : i;
+                  const waitingTime = i === 0 ? 1 : ++i;
                   let auction = currentItem._auction;
                   let currentBid = auction.currentBid || auction.startingBid;
 

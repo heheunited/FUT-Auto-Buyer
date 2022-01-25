a93554f1c0afe7456858e1654380a96e2caedc69
heheunited <lycantrop11@gmail.com>
2022-01-25 21:24:01 +0200

    refactor Log

--- app/handlers/autobuyerProcessor.js
+++ app/handlers/autobuyerProcessor.js
@@ -171,7 +171,7 @@ export const stopAutoBuyer = (isPaused) => {
     .html(isPaused ? "PAUSED" : "IDLE");
 
   if (!isPaused) {
-    let summaryStatsMsg = getSummaryTransferListStats(true);
+    let summaryStatsMsg = !(getBuyerSettings()['idAbErrorsBotNotification']) ? getSummaryTransferListStats(true) : '';
     sendErrorNotificationToUser('Autobuyer go IDLE. ' + summaryStatsMsg);
     writeToLog(summaryStatsMsg, idProgressAutobuyer);
   }

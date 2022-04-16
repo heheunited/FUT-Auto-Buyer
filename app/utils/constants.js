412b4184b4b6df813aacab9308d0dcf7c1173944
heheunited <lycantrop11@gmail.com>
2022-04-16 03:21:18 +0300

    some ref

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,3 +2,5 @@ export const SELL_MOD_BY_COUNT = 3;
 export const SELL_MOD_AFTER_BOT_PAUSE = 2;
 export const SELL_MOD_AUTO_DEFAULT = 1;
 export const SELL_MOD_DISABLED = 4;
+
+export const TRANSFER_LIST_MAX_COUNT = 100;

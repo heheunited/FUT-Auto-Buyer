07f1dd471cb169756a8d1f3d657472a1dfab96dc
heheunited <lycantrop11@gmail.com>
2022-04-21 20:53:20 +0300

    ref

--- app/views/layouts/HeaderView.js
+++ app/views/layouts/HeaderView.js
@@ -66,31 +66,31 @@ export const HeaderView = () => {
   </div>
      <div class="view-navbar-clubinfo">
     <div class="view-navbar-clubinfo-data">
-       <span class="view-navbar-clubinfo-name">Errors: <span id=${id24hTlErrors}></span></span>
+       <span class="view-navbar-clubinfo-name">Errors : <span id=${id24hTlErrors}></span></span>
        <span class="view-navbar-clubinfo-name">Captcha: <span id=${id24hTlCaptcha}></span></span>
     </div>
   </div>
   <div class="view-navbar-currency" style="margin-left: 10px;">
-    <div class="view-navbar-currency-coins">Coins: <span  id=${idAbCoins}></span></div>
-    <div class="view-navbar-currency-coins">Est. Profit: <span  id=${idAbProfit}></span></div>
+    <div class="view-navbar-currency-coins">Coins         : <span  id=${idAbCoins}></span></div>
+    <div class="view-navbar-currency-coins">Est. Profit   : <span  id=${idAbProfit}></span></div>
     <div class="view-navbar-currency-coins">Last TL Profit: <span  id=${idAbLastProfit}></span></div>
   </div>
   <div class="view-navbar-clubinfo">
     <div class="view-navbar-clubinfo-data">
-       <span class="view-navbar-clubinfo-name">Won items: <span id=${idLastWonItemsCount}></span></span>
+       <span class="view-navbar-clubinfo-name">Won items   : <span id=${idLastWonItemsCount}></span></span>
        <span class="view-navbar-clubinfo-name">[<]  Max Bid: <span id=${idLastLessMaxBidItemsCount}></span></span>
        <span class="view-navbar-clubinfo-name">[>=] Max Bid: <span id=${idLastGreaterMaxBidItemsCount}></span></span>
     </div>
   </div>
   <div class="view-navbar-clubinfo">
     <div class="view-navbar-clubinfo-data">
-       <span class="view-navbar-clubinfo-name">Sold Items: <span id=${idAbSoldItems}></span></span>
+       <span class="view-navbar-clubinfo-name">Sold Items  : <span id=${idAbSoldItems}></span></span>
        <span class="view-navbar-clubinfo-name">Unsold Items: <span id=${idAbUnsoldItems}></span></span>
     </div>
   </div>
   <div class="view-navbar-clubinfo" style="border: none;">
     <div class="view-navbar-clubinfo-data">
-       <span class="view-navbar-clubinfo-name">Available Items: <span id=${idAbAvailableItems}></span></span>
+       <span class="view-navbar-clubinfo-name">Available Items : <span id=${idAbAvailableItems}></span></span>
        <span class="view-navbar-clubinfo-name">Active transfers: <span id=${idAbActiveTransfers}></span></span>
     </div>
   </div>`;

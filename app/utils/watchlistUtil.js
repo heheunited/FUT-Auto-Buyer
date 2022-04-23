1f8b48dad1c123b76df482aa9bdcb05aaf78257a
heheunited <lycantrop11@gmail.com>
2022-04-23 03:12:50 +0300

    sm ref

--- app/utils/watchlistUtil.js
+++ app/utils/watchlistUtil.js
@@ -191,9 +191,9 @@ export const watchListUtil = function (buyerSetting) {
                                     if (buyerSetting['idAbPreventTransferListOverflow']) {
                                         let totalItemsCount = getTransferListTotalItemsCount();
 
-                                        itemsLength = (totalItemsCount + itemsLength) <= TRANSFER_LIST_MAX_COUNT
-                                            ? itemsLength
-                                            : ((TRANSFER_LIST_MAX_COUNT - totalItemsCount) - 1);
+                                        if (totalItemsCount + itemsLength >= TRANSFER_LIST_MAX_COUNT) {
+                                            itemsLength = TRANSFER_LIST_MAX_COUNT - totalItemsCount - 2;
+                                        }
 
                                         if (itemsLength !== boughtItems.length) {
                                             writeToLog(`PREVENT TRANSFER LIST OVERFLOW ACTIVATED. OLD ITEMS COUNT: ${boughtItems.length}. NEW ITEMS COUNT: ${itemsLength >= 0 ? itemsLength : 0}.`, idProgressAutobuyer, "\n");

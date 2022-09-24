6fadf4b06a3a39fded050bf959b39b7b4a5100e5
hehe <i.bilyk@yahoo.com>
2022-09-25 01:03:02 +0300

    fifa 23

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,23 +2,30 @@ module.exports = {
   headers: {
     name: "FUT Auto Buyer",
     namespace: "http://tampermonkey.net/",
-    version: "1.2.2",
+    version: "1.2.6",
     description: "FUT Auto Buyer",
-    author: "ZZ",
+    author: "CK Algos",
     match: [
       "https://www.ea.com/*/fifa/ultimate-team/web-app/*",
       "https://www.ea.com/fifa/ultimate-team/web-app/*",
     ],
     grant: ["GM_xmlhttpRequest"],
-    connect: ["ea.com", "ea2.com", "futbin.com", "discordapp.com"],
-    require: [
-      "https://github.com/chithakumar13/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
-      "https://raw.githubusercontent.com/discordjs/discord.js/webpack/discord.11.6.4.min.js",
+    connect: [
+      "ea.com",
+      "ea2.com",
+      "futbin.com",
+      "discordapp.com",
+      "futbin.org",
     ],
-    // updateURL:
-    //   "https://github.com/chithakumar13/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
-    // downloadURL:
-    //   "https://github.com/chithakumar13/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
+    require: [
+      "https://code.jquery.com/jquery-3.6.1.min.js",
+      "https://github.com/ckalgos/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
+      "https://cdn.glitch.me/56b60005-30df-4a91-85e4-899f6c611814/discord.11.4.2.min.js",
+    ],
+    updateURL:
+        "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
+    downloadURL:
+        "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
     noFrame: true,
   },
 };
